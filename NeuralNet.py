fde323eba6084b8c11b5d3f4c92a5d5afde45519
WuYunjin <wwuyunjin@gmail.com>
2020-05-28 20:39:05 +0800

    scaling the training dataset

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- coding: UTF-8 -*-
 # author : yunjin zhaolong
 from __future__ import print_function
@@ -16,7 +15,7 @@ class MLP(nn.Module):
         super(MLP, self).__init__()
 
         self.hidden_1 = torch.nn.Linear(n_feature, n_hidden)  # hidden layer
-        self.bn1 = torch.nn.BatchNorm1d(n_hidden)
+        
 
         self.hidden_2 = torch.nn.Linear(n_hidden, n_hidden//2)
 
@@ -33,7 +32,7 @@ class MLP(nn.Module):
     def forward(self, x):
 
         x = torch.relu(self.hidden_1(x))  # activation function for hidden layer
-        x = self.bn1(x)
+        
 
         x = torch.relu(self.hidden_2(x))  # activation function for hidden layer
 
@@ -56,23 +55,34 @@ class myDataset(Dataset):
         data = pd.read_csv(filepath,delimiter=',',index_col=0,usecols=(1,5))  # Only use Close.Price for now.
 
         if flag == 'train':
-            self.x =  data[-1011:-252] # Price from 2014-1-2 to 2016-12-30
-            self.y = data[-1011+day:-252+day]  # Price from 2014-1-2+day to 2016-12-30+day
+            x =  data[-1011:-252].values # Price from 2014-1-2 to 2016-12-30
+            y = data[-1011+day:-252+day].values  # Price from 2014-1-2+day to 2016-12-30+day
+
+
+            from sklearn.preprocessing import MinMaxScaler
+            sc = MinMaxScaler()
+            x = np.reshape(x,(-1,1)) # For scaling
+            x = sc.fit_transform(x)
+            x = np.reshape(x, (len(x),1,1))
+
+            y = np.reshape(y,(-1,1)) # For scaling
+            y = sc.fit_transform(y)
+
         elif flag == 'test':
-            self.x = data[-252:-day]  # Price from 2017-1-3 to 2017-12-29 - day.
-            self.y = data[-252+day:]  # Price from 2017-1-3 + day to 2017-12-29 .
- 
-        
+            x = data[-252:-day].values  # Price from 2017-1-3 to 2017-12-29 - day.
+            y = data[-252+day:].values  # Price from 2017-1-3 + day to 2017-12-29 .
+
+        self.x = x
+        self.y = y
 
     def __len__(self):
         return len(self.x)
    
     def __getitem__(self, idx):
         
-        x = torch.tensor(self.x.iloc[idx]) # Price at T day
-        y = torch.tensor(self.y.iloc[idx]) # Price at T+self.day day
+        x = torch.tensor(self.x[idx]).float() # Price at T day
+        y = torch.tensor(self.y[idx]).float() # Price at T+self.day day
         return x,y  
-
 
 def train(epochs,model):
     L = []
@@ -101,12 +111,15 @@ def train(epochs,model):
     import matplotlib.pyplot as plt 
     plt.plot(np.array(L))
     plt.savefig(os.path.join(train_output , "hidden_size({})_mse({}).png".format(4 , best_loss))) #文件命名以 hyperparameter_metrice 形式
+    plt.close()
     return model 
 
 
 def test(model):
     model.eval()
     test_loss = 0
+    y1 = np.array([])
+    y2 = np.array([])
     with torch.no_grad():
         for batch_idx, (x,y) in enumerate(test_loader):
             x = x.to(device)
@@ -114,9 +127,17 @@ def test(model):
             loss = loss_function(pred_y,y)
             test_loss += loss
                 
-            
+            y1 = np.append(y1,y.numpy())
+            y2 = np.append(y2,pred_y.numpy())
         print('test set Average loss: {:.4f}'.format(test_loss / len(test_loader.dataset)))
-    
+            
+        import matplotlib.pyplot as plt 
+        plt.plot(y1,label='real',color='blue')
+        plt.plot(y2,label='pred',color='red')
+        plt.legend()
+        plt.savefig(os.path.join("./testset_prediction_vs_real.png"))
+        plt.close()
+
 
 
 torch.manual_seed(1234)
@@ -145,7 +166,7 @@ for ind in range(len(trainfiles_3m)):
         mlp.init_weight() # 增加初始化
         optimizer = optim.SGD(mlp.parameters(), lr=0.001)
 
-        mlp = train(epochs=200, model = mlp)
+        mlp = train(epochs=500, model = mlp)
         test(model = mlp)
         # torch.save(mlp,'E:/BaiduNetdiskDownload/compeition_sigir2020/%sday_%s_model'%(i,trainfiles_3m[ind].split('_')[0].strip('3M')))
         
