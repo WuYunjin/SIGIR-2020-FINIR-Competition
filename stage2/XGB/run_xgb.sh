84e67ed22c0ff81c0989d2840389a79cf1df290f
joelonglin <joelonglin@foxmail.com>
2020-06-28 11:39:05 +0800

    add some description for run_xgb,sh

--- stage2/XGB/run_xgb.sh
+++ stage2/XGB/run_xgb.sh
@@ -1,12 +1,13 @@
-# parser = argparse.ArgumentParser(description="xgboost")
-# parser.add_argument('-t' ,'--train_data_len' , type=int , help='训练长度', default=500)
-# parser.add_argument('-v','--valdata_len', type=str, help='迭代时的长度',default=20)
-# parser.add_argument('-d','--max_depth', type=int, help='xgb max_length', default=7)
-# parser.add_argument('-e'  ,'--eta' , type = float , help = 'xgb eta' , default=0.1)
-# parser.add_argument('-g'  ,'--gamma' , type = float , help = 'xgb gamma' , default=0.3)
+# parser.add_argument('-t' ,'--train_data_len' , type=int , help='training len', default=500)
+# parser.add_argument('-v','--valdata_len', type=int, help='valuation len',default=1)
 # parser.add_argument('-p'  ,'--prob' , type = float , help = 'xgb prob output threshold' , default=0.5)
-# parser.add_argument('-n' , '--n_estimator' , type=int  , help='num of the estimate time' , default=100)
-# args = parser.parse_args()
+# parser.add_argument('-d','--max_depth', type=int, help='xgb max_length', default=8)
+# parser.add_argument('-e'  ,'--eta' , type = float , help = 'xgb eta' , default=0.01)
+# parser.add_argument('-g'  ,'--gamma' , type = float , help = 'xgb gamma' , default=0.0)
+# parser.add_argument('-m'  ,'--metal' , type = int , help = 'index of the target metal' , default=0)
+# parser.add_argument('-n' , '--n_estimators' , type=int  , help='num of the estimate time' , default=50)
+# parser.add_argument('-D' , '--day' , type=int  , help='day to predict' , default=1)
+# parser.add_argument('-l' , '--log' , type=float  , help='log the result better than this value' , default=0.55)
 n_estimators='50'
 eta='0.01'
 depth='10'
